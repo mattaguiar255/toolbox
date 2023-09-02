d0f37e58f1b65c31e9c249d2e4b26eda5b073811
Matt Aguiar <mattaguiar255@protonmail.ch>
2023-09-02 12:04:34 -0400

    Display Fetched Tool as Card
    This commit further expands upon the Home route by fetching
    the tools from the server and displaying one of them as a
    Card. The next commit will complete the task of displaying all
    tools as cards in the Home route.

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,23 +1,32 @@
+import { useRef, useState } from "react";
 import Card from "../../components/Card/Card";
 import styles from "./Home.module.css"
 
-// This is a temporary fetch for testing the PHP backend.
-fetch(process.env.REACT_APP_SERVER_URL_PHP + "/GetAllTools.php").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
-
 function Home(): React.ReactElement {
+
+  // Ideally you'd want an Array<Tool> but time is not allowing for the
+  // definition of a full Tool interface.
+  const [tools, setTools] = useState<Array<any> | null>(null);
+  const hasMounted = useRef<boolean>(false);
+
+  if (hasMounted.current === false) {
+    hasMounted.current = true;
+    fetch(process.env.REACT_APP_SERVER_URL_PHP + "/GetAllTools.php").then((response) => {
+      response.json().then((data) => setTools(data));
+    });
+  }
 
   return (
     <div id={styles.cardFlexbox}>
-      <Card
-        title="TypeScript"
-        img="https://pbs.twimg.com/profile_images/1648471227416346625/v84A9gXA_400x400.png"
-        shortDescription="JavaScript with Types"
-        linkURL="https://www.typescriptlang.org/"
-      />
+      { 
+        tools !== null &&
+        <Card
+          title={tools[1].title}
+          img={tools[1].icon_link}
+          shortDescription={tools[1].short_desc}
+          linkURL={tools[1].docs_link}
+        />
+      }
     </div>
   );
 
