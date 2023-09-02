020c2af9a75205f1056f65c09d3500a538015ae6
Matt Aguiar <mattaguiar255@protonmail.ch>
2023-09-02 00:51:51 -0400

    Testing the PHP Backend
    May want to see commit 946d25ce7e04f6d86cc077445afc273165b86191
    from the toolbox-backend repo before reading this.
    
    This commit tests if the GetAllTools.php endpoint works properly.
    Eventually that endpoint will be used to populate cards on the
    /src/routes/Home.tsx route.
    
    A demo fetch call has been made at the top of /src/routes/Home.tsx.
    Notice the:
    
    process.env.REACT_APP_SERVER_URL_PHP + "/GetAllTools.php"
    
    This uses the REACT_APP_SERVER_URL_PHP environment variable
    found in both /.env.development and /.env.production. Notice
    how they have different values in each file. This is because
    on the localhost server, the app is not being hosted from our
    Apache2 web server. Thus, the REACT_APP_SERVER_URL in
    /.env.development MUST be changed such that the IP address
    or domain points to the machine Apache2 and the PHP files
    are hosted on. The REACT_APP_SERVER_URL in /.env.production
    must NOT be changed as the production code will be run from
    the same machine that Apache2 runs on and the same directory
    Apache2 is serving. Hence, the REACT_APP_SERVER_URL=.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,11 @@
 
+// This is a temporary fetch for testing the PHP backend.
+fetch(process.env.REACT_APP_SERVER_URL_PHP + "/GetAllTools.php").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+
 function Home(): React.ReactElement {
   return <div>This is the home page.</div>;
 }
